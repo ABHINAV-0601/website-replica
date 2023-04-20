a38968bcbcf795f132dee85c0398a8c6a1feac7d
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2023-04-20 12:53:29 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { CourseComponent } from './course/course.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { FooterComponent } from './footer/footer.component';
+import { InstructorComponent } from './instructor/instructor.component';
+import { FAQComponent } from './faq/faq.component';
+import { HowToApplyComponent } from './how-to-apply/how-to-apply.component';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CourseComponent,
+    ServicesComponent,
+    AboutUsComponent,
+    FooterComponent,
+    InstructorComponent,
+    FAQComponent,
+    HowToApplyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScrollToModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
